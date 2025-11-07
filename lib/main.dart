1656d50be7629011c242ea70edbd9628c9768153
Reyza <abdulreyza6@gmail.com>
2025-11-07 11:09:27 +0700

    create page splash screen scaffold awal

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyWidget extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: MySplashScreen(),
     );
   }
 }
