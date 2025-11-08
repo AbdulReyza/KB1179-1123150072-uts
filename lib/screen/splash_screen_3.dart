18d00b216e7f39543f960cdc857c4a5094fb1791
AbdulReyza <abdulreyza6@gmail.com>
2025-11-08 13:29:24 +0700

    buat stl login

---
+++ lib/screen/splash_screen_3.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class MySplashScreen3 extends StatelessWidget {
+  const MySplashScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange.shade50,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                   children: [
+                    SizedBox(height: 200),
+                    Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.deepOrange,
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/chennai-india-april-20-2023-260nw-2291474419.jpg"),
+                          fit: BoxFit.cover,
+                    )
+                  ),
+                ),
+
+                Text('Get Started',
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Text('Explore Flavor and Excitement',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.deepOrange
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.deepOrange.shade300
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.deepOrange.shade300
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.deepOrange
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Container(
+                    child: SizedBox(
+                      height: 40,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: (){
+                      
+                        }, 
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.deepOrange,
+                        ),
+                        child: Text('Continue',
+                        style: TextStyle(
+                          color: Colors.white
+                        ),),
+                        
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+
+                  ],
+              ),
+            ),
+          );();
+    
+  }
+}
